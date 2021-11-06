ab966f0d1b31736529853b3bbd4ddd83195dcdf3
SebTrill <89354578+SebTrill@users.noreply.github.com>
2021-11-05 19:44:25 -0500

    Add files via upload
    Added the file containing the visual studio file

---
+++ Hack K-State Project/SchedulerPro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchedulerPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
