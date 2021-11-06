befd39e7a8644d9370d96d94b3dbdeb5ebbfa417
SebTrill <89354578+SebTrill@users.noreply.github.com>
2021-11-05 21:15:25 -0500

    Add files via upload
    Added the SchedulerPro 2.0

---
+++ Hack K-State Project/SchedulerPro 2.0/SchedulerPro 2.0/Scheduler.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace SchedulerPro_2._0
+{
+    partial class uxUserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxOpenFile = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.uxMondayText = new System.Windows.Forms.TextBox();
+            this.uxTuesdayText = new System.Windows.Forms.TextBox();
+            this.uxWednesdayText = new System.Windows.Forms.TextBox();
+            this.uxThurdayText = new System.Windows.Forms.TextBox();
+            this.uxFridayText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxOpenFile
+            // 
+            this.uxOpenFile.Location = new System.Drawing.Point(208, 1266);
+            this.uxOpenFile.Name = "uxOpenFile";
+            this.uxOpenFile.Size = new System.Drawing.Size(325, 67);
+            this.uxOpenFile.TabIndex = 0;
+            this.uxOpenFile.Text = "Open File";
+            this.uxOpenFile.UseVisualStyleBackColor = true;
+            this.uxOpenFile.Click += new System.EventHandler(this.uxOpenFile_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.uxMondayText);
+            this.flowLayoutPanel1.Controls.Add(this.uxTuesdayText);
+            this.flowLayoutPanel1.Controls.Add(this.uxWednesdayText);
+            this.flowLayoutPanel1.Controls.Add(this.uxThurdayText);
+            this.flowLayoutPanel1.Controls.Add(this.uxFridayText);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 88);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(2810, 953);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.label3);
+            this.flowLayoutPanel2.Controls.Add(this.label4);
+            this.flowLayoutPanel2.Controls.Add(this.label5);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(2810, 70);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // uxMondayText
+            // 
+            this.uxMondayText.Location = new System.Drawing.Point(3, 3);
+            this.uxMondayText.Name = "uxMondayText";
+            this.uxMondayText.Size = new System.Drawing.Size(555, 38);
+            this.uxMondayText.TabIndex = 0;
+            this.uxMondayText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // uxTuesdayText
+            // 
+            this.uxTuesdayText.Location = new System.Drawing.Point(564, 3);
+            this.uxTuesdayText.Name = "uxTuesdayText";
+            this.uxTuesdayText.Size = new System.Drawing.Size(555, 38);
+            this.uxTuesdayText.TabIndex = 1;
+            // 
+            // uxWednesdayText
+            // 
+            this.uxWednesdayText.Location = new System.Drawing.Point(1125, 3);
+            this.uxWednesdayText.Name = "uxWednesdayText";
+            this.uxWednesdayText.Size = new System.Drawing.Size(555, 38);
+            this.uxWednesdayText.TabIndex = 2;
+            // 
+            // uxThurdayText
+            // 
+            this.uxThurdayText.Location = new System.Drawing.Point(1686, 3);
+            this.uxThurdayText.Name = "uxThurdayText";
+            this.uxThurdayText.Size = new System.Drawing.Size(555, 38);
+            this.uxThurdayText.TabIndex = 3;
+            // 
+            // uxFridayText
+            // 
+            this.uxFridayText.Location = new System.Drawing.Point(2247, 3);
+            this.uxFridayText.Name = "uxFridayText";
+            this.uxFridayText.Size = new System.Drawing.Size(555, 38);
+            this.uxFridayText.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(185, 0, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monday";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(732, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(325, 0, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 63);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tuesday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1254, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(285, 0, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(312, 63);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wednesday";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1844, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(275, 0, 3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 63);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Thursday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(2424, 0);
+            this.label5.Margin = new System.Windows.Forms.Padding(325, 0, 3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 63);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Friday";
+            // 
+            // uxOpenFileDialog
+            // 
+            this.uxOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // uxUserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(2833, 1537);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.uxOpenFile);
+            this.Name = "uxUserInterface";
+            this.Text = "SchedulerPro";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxOpenFile;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox uxMondayText;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TextBox uxTuesdayText;
+        private System.Windows.Forms.TextBox uxWednesdayText;
+        private System.Windows.Forms.TextBox uxThurdayText;
+        private System.Windows.Forms.TextBox uxFridayText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
+    }
+}
+
